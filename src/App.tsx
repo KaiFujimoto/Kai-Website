df1f109e88d5fea9e5bebc11978bee6d97d0c3ca
Kai Fujimoto <kaifujidesu@gmail.com>
2025-04-23 23:05:12 -0400

    feature: using context hooks example via dark mode and light mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext, useState, useContext } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -8,20 +8,49 @@ import MyExperience from './components/MyExperience';
 import MySkills from './components/MySkills';
 import MyEducation from './components/MyEducation';
 
+interface ThemeContextType {
+  isDarkMode: boolean;
+  setIsDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
 function App() {
+	
+	const [isDarkMode, setIsDarkMode] = useState(false);
+
   return (
-    <div className="App">
-      <Header />
-			<div className="app-body">
-				<AboutMe />
-				<MyExperience />
-				<MySkills />
-				<MyEducation />
-				<EmailMe />
- 			</div>
-			<Footer />
-		</div>
+		<ThemeContext.Provider value={{ isDarkMode, setIsDarkMode }}>
+			<div className={`${isDarkMode ? "dark" : "light"} App`}>
+			  <ThemeToggle />	
+				<Header />
+				<div className="app-body">
+					<AboutMe />
+					<MyExperience />
+					<MySkills />
+					<MyEducation />
+					<EmailMe />
+				</div>
+				<Footer />
+			</div>
+		</ThemeContext.Provider>
   );
+}
+
+function ThemeToggle() {
+	const context = useContext(ThemeContext);
+	
+	if (!context) {
+		return null;
+	}
+	
+	const { isDarkMode, setIsDarkMode } = context;
+
+	return (
+		<button className={isDarkMode ? "light" : "dark" } onClick={() => setIsDarkMode(!isDarkMode)}>
+			Toggle to {isDarkMode ? "Light" : "Dark"} Mode!
+		</button>
+	);
 }
 
 export default App;
